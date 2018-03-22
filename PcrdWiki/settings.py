54df536b0202fbd92c101f0f71bcebe0b1e161e9
John Lyu <paleneutron@outlook.com>
2018-03-22 12:26:25 +0800

    add static root

--- PcrdWiki/settings.py
+++ PcrdWiki/settings.py
@@ -136,9 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'pcrd_db'),
     os.path.join(BASE_DIR, 'unpacked_asset')
 ]
 
