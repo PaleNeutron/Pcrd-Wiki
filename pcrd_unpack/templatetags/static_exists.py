935173ec5bcba2167f2e8df3fedcb9c67435541c
John Lyu <paleneutron@outlook.com>
2018-03-24 19:01:10 +0800

    static finder for deployed server

--- pcrd_unpack/templatetags/static_exists.py
+++ pcrd_unpack/templatetags/static_exists.py
@@ -1,13 +1,15 @@
+import os
 from django import template
 from django.core.files.storage import default_storage
 from django.contrib.staticfiles import finders
+from django.conf import settings
 from django.contrib.staticfiles.templatetags.staticfiles import static
 register = template.Library()
 
 @register.filter
 def static_exists(path):
-    # path =static(path)
-    if finders.find(path[1:].split("/", 1)[1]):
+    srt = settings.STATIC_ROOT
+    if finders.find(path[1:].split("/", 1)[1]) or os.path.exists(os.path.join(srt, path[1:])):
         return True
     else:
         return False
