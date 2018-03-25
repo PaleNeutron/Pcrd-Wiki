2e4696ba30c861c7a566f90d510e330bd42d18d9
John Lyu <paleneutron@outlook.com>
2018-03-25 14:00:10 +0800

    not overwrite during ensure statics

--- pcrd_unpack/management/commands/ensure_statics.py
+++ pcrd_unpack/management/commands/ensure_statics.py
@@ -19,16 +19,17 @@ class Command(BaseCommand):
         self.get_img([r"unpacked_asset\Texture2D\assets\_elementsresources\resources\unit\profile/"], force43=True)
         self.get_img([r"unpacked_asset\Texture2D\assets\_elementsresources\resources\unit\actualprofile/"], force43=True)
 
-    def get_img(self, img_dirs, force43=False, fill_color="#fff"):
+    def get_img(self, img_dirs, force43=False, fill_color="#fff", force=False):
         for d in img_dirs:
             new_dir = os.path.join(self.target_static_dir, re.split(r"/|\\", d, 1)[1])
             os.makedirs(os.path.join(new_dir, ""), exist_ok=True)
             # distutils.dir_util.copy_tree(d, new_dir)
             for f in os.listdir(d):
-                "ad".split()
+                outfile = os.path.join(new_dir, f.replace("png", "jpg"))
+                if os.path.exists(outfile) and not force:
+                    continue
                 im = Image.open(os.path.join(d,f))
                 im.convert("RGB")
-                outfile = os.path.join(new_dir, f.replace("png", "jpg"))
 
                 if im.mode in ('RGBA', 'LA'):
                     background = Image.new(im.mode[:-1], im.size, fill_color)
