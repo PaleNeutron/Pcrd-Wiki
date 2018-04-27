4c0be672c2d0a1e97b27ef225b07aa39cd16456e
John Lyu <paleneutron@outlook.com>
2018-04-27 15:20:29 +0800

    improve skil factor caluate && add atk pattern table

--- pcrd_unpack/views/views.py
+++ pcrd_unpack/views/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, View, ListView, DetailView
 from django.shortcuts import get_list_or_404, get_object_or_404
+from itertools import zip_longest
 from django.urls import reverse
 # Create your views here.
 from pcrd_unpack import models
@@ -120,6 +121,36 @@ class UnitDetailView(TemplateView):
         context["unit_profile"] = unit_profile
         context["unit_promotion"] = unit_promotion
         context["unit_skills"] = unit_skills
+        ur = models.UnitRarity
+        properties = [
+            ur.hp, ur.hp_recovery_rate, ur.wave_hp_recovery,
+            ur.energy_recovery_rate, ur.wave_energy_recovery,
+            ur.atk, ur.magic_str,
+            ur.def_field, ur.magic_def,
+            ur.physical_critical, ur.magic_critical,
+            ur.dodge,
+            ur.life_steal,
+        ]
+        context["data_tags"] = [property.field_name for property in properties]
+        context["table_data_tags"] = zip_longest(*[iter(context["data_tags"])] * 3, fillvalue=None)
+        unit_pattern = get_object_or_404(models.UnitAttackPattern, unit_id=unit_id)
+        # patterns = [
+        #     getattr(unit_pattern, f.name)
+        #     for f in type(unit_skill)._meta.get_fields()
+        #     if (not f.primary_key and f.name.startswith("atk_pattern_"))
+        # ]
+        patterns = []
+        for i in range(unit_pattern.loop_end):
+            p = getattr(unit_pattern, 'atk_pattern_{}'.format(i+1))
+            if p not in [0, 1]:
+                skill_pattern = int(unit_pattern.unit_id / 100) * 1000 + p - 1000 + 1
+                p = get_object_or_404(models.SkillData, skill_id=skill_pattern).name
+            patterns.append(p)
+
+        context['unit_patterns'] = {
+            'loop_start': unit_pattern.loop_start,
+            'patterns': patterns,
+        }
 
         return context
 
@@ -128,12 +159,58 @@ class UnitDetailView(TemplateView):
                    for f in type(skill)._meta.get_fields()
                    if not f.primary_key and f.name.startswith("action") and getattr(skill, f.name) != 0]
         for a in actions:
-            if a.action_type in [90, 26]:
-                a.action_value_1 = a.action_value_2
-                a.action_value_2 = a.action_value_3
-                a.action_value_3 = 0
-                a.action_value_4 = 0
+
+            if a.action_type in [1, 8, 9, 11, 12, 13, 25, 32, 38, 39, 40, 47,]:
+                factor_static = a.action_value_1
+                factor_level = a.action_value_2
+                factor_atk = a.action_value_3
+                factor_atk_level = a.action_value_4
+            elif a.action_type in [4, 10, 24, 29, 31, 34,]:
+                factor_static = a.action_value_2
+                factor_level = a.action_value_3
+                factor_atk = a.action_value_4
+                factor_atk_level = a.action_value_5
+            elif a.action_type in [5, 6, 14, 16, 17, 18, 19, 20, 21, 33, 46, ]:
+                factor_static = a.action_value_1
+                factor_level = a.action_value_2
+                factor_atk = 0
+                factor_atk_level = 0
+            elif a.action_type in [90, 91, 26, 15, 27]:
+                factor_static = a.action_value_2
+                factor_level = a.action_value_3
+                factor_atk = 0
+                factor_atk_level = 0
+            else:
+                factor_static = 0
+                factor_level = 0
+                factor_atk = 0
+                factor_atk_level = 0
+
+
+            # manual patches
+            if a.action_type == 2:
+                a.description = 'Move'
+                factor_static = a.action_value_1
+
+            elif a.action_type == 3:
+                a.description = 'Push'
+                factor_static = a.action_value_3
+
+
+            a.result = ""
+            if factor_static:
+                a.result += str(factor_static)
+            if factor_level:
+                a.result += " + {} × skill_level".format(factor_level)
+            if factor_atk_level and factor_atk:
+                a.result += " + ({} + {} × skill_level) × atk".format(factor_atk, factor_atk_level)
+            elif factor_atk:
+                a.result += " + {} × atk".format(factor_atk)
+
+
+
         return actions
+
 
 def handler404(request, exception):
     return render(request, 'pcrd_unpack/errors/404.html', locals())
