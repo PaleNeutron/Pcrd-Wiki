e5a164c896893b09b6e2e296893f5f074a425ec0
John Lyu <paleneutron@outlook.com>
2018-05-28 09:40:53 +0800

    add area sitemap

--- pcrd_unpack/sitemaps.py
+++ pcrd_unpack/sitemaps.py
@@ -1,5 +1,5 @@
 from django.contrib.sitemaps import Sitemap
-from pcrd_unpack.models import UnitProfile, UnitData, EquipmentData
+from pcrd_unpack.models import UnitProfile, UnitData, EquipmentData, QuestAreaData
 from django.urls import reverse
 import datetime
 
@@ -8,7 +8,7 @@ class UnitSitemap(Sitemap):
     priority = 1
 
     def items(self):
-        return UnitData.objects.exclude(comment__isnull=True)
+        return UnitData.objects.order_by('unit_id').exclude(comment__isnull=True)
 
     def lastmod(self, obj):
         # todo add data version include date to models
@@ -22,7 +22,7 @@ class EquimpmentSitemap(Sitemap):
     priority = 1
 
     def items(self):
-        return EquipmentData.objects.all()
+        return EquipmentData.objects.order_by('equipment_id')
 
     def lastmod(self, obj):
         # todo add data version include date to models
@@ -30,3 +30,17 @@ class EquimpmentSitemap(Sitemap):
 
     def location(self, obj):
         return reverse('pcrd_unpack:equipment', kwargs={"equipment_id": obj.equipment_id})
+
+class QuestAreaSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 1
+
+    def items(self):
+        return QuestAreaData.objects.order_by('area_id').filter(area_id__lt=20000)
+
+    def lastmod(self, obj):
+        # todo add data version include date to models
+        return datetime.datetime.today()
+
+    def location(self, obj):
+        return reverse('pcrd_unpack:area_detail', kwargs={"area_id": obj.area_id})
